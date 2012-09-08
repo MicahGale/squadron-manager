c291b3f89197aef517f3d110d75c270abc4d47dd
micah.gale <Micah.Gale@gmail.com>
2012-09-08 03:11:55 +0000

    Initial Upload /signIn/logout.php

---
+++ signIn/logout.php
@@ -0,0 +1,28 @@
+<?php
+/* * Copyright 2012 Micah Gale
+ *
+ * This file is a part of Squadron Manager
+ *
+ *Squadron Manager is free software licensed under the GNU General Public License version 3.
+ * You may redistribute and/or modify it under the terms of the GNU General Public License
+ * version 3 as published by the Free Software Foundation.
+ *
+ * Squadron Manager comes without a warranty; without even the implied warranty of merchantability
+ * or fitness for a particular purpose. See the GNU General Public License version 3 for more
+ * details.
+ *
+ * You should have received the GNU General Public License version 3 with this in GPL.txt
+ * if not it is available at <http://www.gnu.org/licenses/gpl.txt>.
+ *
+ * 
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <meta HTTP-EQUIV="REFRESH" content="0; url=../index.php">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <?php
+    session_start();
+    session_destroy();
+    ?>
+</html>
