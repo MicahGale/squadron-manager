6e52435e3e2fa06533624274f34555a3d3676932
micah.gale <Micah.Gale@gmail.com>
2012-09-08 03:10:34 +0000

    Initial Upload /signIn/edit.php

---
+++ signIn/edit.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Edit Personal Information</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="shortcut icon" href="../patch.ico">
+    </head>
+    <body>
+        <?php
+        include("header.php");
+        include("projectFunctions.php");
+        session_start();
+        if(!array_key_exists("member",$_SESSION)) {
+            auditLog($_SERVER["REMOTE_ADDR"],"DC");
+            echo"<meta HTTP-EQUIV=\"REFRESH\" content=\"0; url=../signIn/?CAPID=\">";
+        }
+        ######################################TODO load all dates################################################# 
+       $ident =Connect( 'Sign-in','ab332kj2klnnfwdndsfopi320932i45n425l;kfoiewr','localhost');
+        $member=$_SESSION["member"];
+       $member->init(4,$ident);
+        $member->editInformation($ident);
+        ?>                 
+        <a href="../index.php">go Home</a><br/>
+        <a href="promotionReport.php">View Your Promotion Progress</a> <br/>
+        <a href="logout.php">Logout</a>
+        <?php include("footer.php");?>
+    </body>
+</html>
