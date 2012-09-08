b2ff722f21f7134008a859d36bef6f1d4f783447
micah.gale <Micah.Gale@gmail.com>
2012-09-08 03:02:34 +0000

    Initial Upload author.php

---
+++ authors.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Squadron Manager Developers</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="shortcut icon" href="patch.ico">
+    </head>
+    <body>
+        <?php include('header.php'); ?>
+        <h3>All developers who have contributed to Squadron Manager</h3>
+        <table border="1" cellspacing="1">
+            <tr>
+                <th>Developer Name</th> <th>Email address</th>
+            </tr>
+            <tr>
+                <td>Micah Gale</td> <td><a href="mailto:micah.gale@gmail.com">micah.gale@gmail.com</a></td>
+            </tr>
+        </table>
+        <?php include('footer.php');?>
+    </body>
+</html>
