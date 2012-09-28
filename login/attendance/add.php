b35780daedba936f5a247a79c86a7a7acc3165ea
micah.gale <Micah.Gale@gmail.com>
2012-09-28 13:59:07 +0000

    Started to parse input, and allow to search for members, and save current input

--- login/attendance/add.php
+++ login/attendance/add.php
@@ -19,7 +19,14 @@
 include("projectFunctions.php");
 session_secure_start();
 if(in_array("Search", $_POST)) {             //if searched for member save input, then redirect to search
-    
+    $_SESSION['input']= array();
+   for($i=0;$i<$_POST['number'];$i++) {      //loop through to find which one was searched for and save the input
+       if(isset($_POST["search$i"])&&$_POST["search$i"]=="Search") {  //if was the one searched for
+           
+       } else if(isset($_POST['cap'][$i])&&$_POST['cap'][$i]!="") {  //if the capid wasn't blank then save it
+           
+       }
+   } 
 }
 ?>
 <!DOCTYPE html>
@@ -38,6 +45,7 @@ if(in_array("Search", $_POST)) {             //if searched for member save input
             <strong>Please enter attendance for this event below</strong><br>
             <br>
             <form method="post">
+                <input type="hidden" name="number" value="<?php echo $numberOfInserts;?>"/>
             <?php
             for($i=0;$i<$numberOfInserts;$i++) {  //loop through to create a ton of inputs
                 echo 'Insert by CAPID: <input type="text" name="cap[]" size="5" maxlength="6"/> or <input type="submit" name="search'.$i.'" value="Search"/>'."<br>\n";
