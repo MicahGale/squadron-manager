56e0b439c25e45ae4beaf8b0c1a097b5bb214029
Micah.Gale@gmail.com <Micah.Gale@gmail.com@4421c440-7e73-079f-92d9-bfb8097ecaeb>
2013-06-03 06:36:54 +0000

    Debugged promotion report Debugged emergency Contact Report, and print css changed the login system to one db user and hashes changed to /lib and /etc rather than /share started a page to change the user's password

--- trunk/share/footer.php
+++
@@ -1,9 +0,0 @@
-<table border="0" width="800">
-    <tr>
-        <td align="center">
-            <strong>FOR OFFICIAL CIVIL AIR PATROL USE ONLY</strong> <br>
-            <a href="/legal.php">Privacy and Legal Information</a> <br>
-            <a href="/licensing.php">Copyright and Licensing Information</a>
-        </td>
-    </tr>
-</table>
