4ac6ffdcb533a457e493f610029c6cf7a5e0ae13
Micah.Gale@gmail.com <Micah.Gale@gmail.com@4421c440-7e73-079f-92d9-bfb8097ecaeb>
2013-10-11 05:05:17 +0000

    Pre- beta final changes. Fixed the secure session start. Still needs to finish populating the database.

--- trunk/login/reSignIn.php
+++ trunk/login/reSignIn.php
@@ -1,5 +1,5 @@
 <?php
-/* * Copyright 2012 Micah Gale
+/* * Copyright 2013 Micah Gale
  *
  * This file is a part of Squadron Manager
  *
@@ -16,6 +16,37 @@
  *
  * 
  */
+include("projectFunctions.php");
+session_start();
+if(!isset($_SESSION['resignin'])) { //if session has died
+    header("refresh:0; url=/login/endSession.php");
+    exit;
+}
+if(isset($_SESSION['attempts'])&&$_SESSION['attempts']>=MAX_LOGIN) {
+    log_off ();
+    header("refresh:0; url=/login/endSession.php");
+    exit;
+}
+if(isset($_POST['password'])) { //if has password given
+    $ident=  connect('ViewNext');
+     $passes=  parse_ini_file(PSSWD_INI);
+     $salt=$passes['salt'];
+     $member= $_SESSION['member'];
+     $password=$_POST['password'];
+     if($member->check_password($ident, $password, $salt)&&!$member->check_terminated($ident)) {  //checks the password
+         auditLog($_SERVER['REMOTE_ADDR'],'RS');
+         $_SESSION['resignin']=null;
+         unset($_SESSION['resignin']);  //say signed in fine
+         header("refresh:0; url=/login/home.php");
+         exit;
+    } else {
+        if(!isset($_SESSION['attempts']))  //count number of bad attempts
+            $_SESSION['attempts']=0;
+        else
+            $_SESSION['attempts']++;
+        sleep(BAD_LOGIN_WAIT);        //waits if bad login
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,11 +54,10 @@
         <title>resign-in for security</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel="shortcut icon" href="/patch.ico"/>
+        <script type="text/javascript" src="/java_script/CAPS_LOCKS.js"></script>
     </head>
     <body>
         <?php
-        include("projectFunctions.php");
-        session_start();
         include("header.php");
         ?>
         <p>
@@ -35,9 +65,9 @@
             to gain access to your login).  Please sign-in again below so we may verify that you are who you say you are, and not the hacker.
             Thank you for your patience.
         </p>
-        <form action="redirect.php" method="post">
-            <input type="text" size="5" name="user" value="<?php echo $_SESSION['member']->getCapid(); ?>"/><br>
-            <input type="password" size="5" name="password"/><br>
+        <form method="post">
+            <input type="text" size="5" disabled="disabled" value="<?php echo $_SESSION['member']->getCapid(); ?>"/><br>
+            <input type="password" size="5" name="password" onkeypress="check_caps(event)"/><span id="warn" class="warning"></span><br>
             <input type="submit" value="Sign-in"/>
         </form>
     </body>
